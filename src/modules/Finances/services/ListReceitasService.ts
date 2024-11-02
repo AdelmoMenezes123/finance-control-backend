550c7b1217f64673ba00e057aa3e8e58339ef6e2
Adelmo Menezes <adelmo@connectlab.com.br>
2024-11-01 21:00:59 -0300

    cria service para op modulo de receita

--- src/modules/Finances/services/ListReceitasService.ts
+++ src/modules/Finances/services/ListReceitasService.ts
@@ -3,13 +3,13 @@ import { ICreateReceitaDTO } from "../dtos/ICreateReceitaDTO";
 import { IReceitaRepository } from "../repositories/IReceitaRepository";
 
 @injectable()
-export class CreateReceitaService {
+export class ListReceitasService {
   constructor(
     @inject("ReceitaRepository")
     private receitaRepository: IReceitaRepository
   ) {}
 
-  async execute(data: ICreateReceitaDTO): Promise<ICreateReceitaDTO> {
-    return await this.receitaRepository.create(data);
+  async execute(userId: number, mes: number, ano: number): Promise<ICreateReceitaDTO[]> {
+    return await this.receitaRepository.listByMonthAndYear(userId, mes, ano);
   }
 }
