670973dcc259b4a92584b00d0aaaf11021bb0a0c
Adelmo Menezes <adelmo@connectlab.com.br>
2024-11-02 01:55:52 -0300

    configurando o jest

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,10 @@
 // Configuração do Prisma Client
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.NODE_ENV === "test" ? process.env.DATABASE_URL_TEST : process.env.DATABASE_URL,
+    },
+  },
+});
